f49a8f016191f59879708fd60a1a38a4240839a2
krzysztofzapart <krzysztofzapart@icloud.com>
2021-08-31 11:07:16 +0200

    Initial Commit

---
+++ src/main/java/pl/kzapart/todoList/RESTapi/ResTapiApplication.java
@@ -0,0 +1,13 @@
+package pl.kzapart.todoList.RESTapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTapiApplication.class, args);
+	}
+
+}
