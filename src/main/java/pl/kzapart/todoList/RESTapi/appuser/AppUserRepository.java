57b8fc2436bd61fdafe823614ebf232e590c1850
krzysztofzapart <krzysztofzapart@icloud.com>
2021-08-31 23:36:00 +0200

    Add Spring Security & hotfix adnotations

--- src/main/java/pl/kzapart/todoList/RESTapi/appuser/AppUserRepository.java
+++ src/main/java/pl/kzapart/todoList/RESTapi/appuser/AppUserRepository.java
@@ -1,11 +1,12 @@
 package pl.kzapart.todoList.RESTapi.appuser;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String mail);
 }
--- src/main/java/pl/kzapart/todoList/RESTapi/registration/RegistrationService.java
+++ src/main/java/pl/kzapart/todoList/RESTapi/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package pl.kzapart.todoList.RESTapi.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "works";
